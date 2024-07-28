5c10ca8690f1a35673c331d0dc2a7a0a9524073a
Khannan <sait04khan@gmail.com>
2024-07-28 19:56:52 +0500

    1

---
+++ main.py
@@ -0,0 +1,31 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token="7292194805:AAE1IWLoYpWifhdFS6XGW1elnCRKr7KjuQA")
+dp = Dispatcher()
+
+
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    markup = types.InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                types.InlineKeyboardButton(
+                    text="Demo chat",
+                    web_app=types.WebAppInfo(url='https://moneylex.online:8083/'),
+                )
+            ]
+        ]
+    )
+    await message.answer("Hello!", reply_markup=markup)
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
